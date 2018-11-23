77f0db2af6a4e41a46cf395cb1331ecc76f0de3b
NABLANCH <nathan.blanchard@capgemini.com>
2018-11-23 15:38:48 +0100

    Dit is een test. Aanpassing voor een Maven project

---
+++ src/main/java/Opdracht1Test/MainTest1.java
@@ -0,0 +1,7 @@
+package Opdracht1Test;
+
+public class MainTest1 {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
---
+++ src/main/java/Opdracht2Test/MainTest2.java
@@ -0,0 +1,9 @@
+package Opdracht2Test;
+
+public class MainTest2 {
+
+
+    public static void main(String[] args) {
+        System.out.println("test2");
+    }
+}
