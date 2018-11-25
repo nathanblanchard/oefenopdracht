d69a309eb1e9a7645f463f4cb1a6569299bfaf31
NABLANCH <nathan.blanchard@capgemini.com>
2018-11-25 13:01:52 +0100

    CalenderTest v0.1 | work in progress

---
+++ src/main/java/Opdracht1Test/CalendarTest1.java
@@ -0,0 +1,64 @@
+package Opdracht1Test;
+
+import java.time.LocalDate;
+
+public class CalendarTest1 {
+
+    public static void main(String[] args) {
+
+        LocalDate date = LocalDate.of(2018, 10, 1);
+
+        // print maand
+        System.out.println(date.getMonth());
+
+        //check 1e dag (ma/di/wo) van de maand vanuit LocalDate. Hiermee kun je
+        //vaststellen waar je moet beginnen met het plaatsen in de Array van 6 bij 7.
+//        System.out.println(date.getDayOfWeek());
+        StringBuilder sb1 = new StringBuilder();
+        sb1.append(date.getDayOfWeek());
+        System.out.println(sb1.toString());
+
+        //print dagen in de week
+        StringBuilder sb = new StringBuilder();
+        sb.append("Su ");
+        sb.append("Mo ");
+        sb.append("Tu ");
+        sb.append("We ");
+        sb.append("Th ");
+        sb.append("Fr ");
+        sb.append("Sa");
+        System.out.println(sb.toString());
+
+        String[][] CalendarTest2 = new String[7][6];
+
+        if ("SUNDAY".equals(sb1.toString())) {
+        }
+
+        for (int i = 1; i <= 7; i++) {
+            switch (sb1.toString()) {
+                case "MONDAY":
+                    System.out.print("   " + i);
+                    break;
+                case "TUESDAY":
+                    System.out.println("      " + i);
+                    break;
+                case "WEDNESDAY":
+                    System.out.println("         " + i);
+                    break;
+                case "THURSDAY":
+                    System.out.println("            " + i);
+                    break;
+                case "FRIDAY":
+                    System.out.println("               " + i);
+                    break;
+                case "SATURDAY":
+                    System.out.println("                  " + i);
+                    break;
+                default:
+                    System.out.println("Geen idee");
+                    break;
+            }
+
+        }
+    }
+}
--- src/main/java/Opdracht1Test/MainTest1.java
+++
@@ -1,7 +0,0 @@
-package Opdracht1Test;
-
-public class MainTest1 {
-    public static void main(String[] args) {
-        System.out.println("test");
-    }
-}
